b6e0b5a08c429d038050703a376bbc96308c6921
Matthew Marcellino <matthew.marcellino1123@gmail.com>
2022-04-22 09:30:24 +0700

    Fix Decimals

--- getCurrentSushiswapLiquidity.ts
+++ getCurrentSushiswapLiquidity.ts
@@ -1,5 +1,9 @@
-const { ethers } = require('ethers');
+const { ethers } = require("ethers");
 const jsonABI = require("./abi/gohm-eth-abi.json");
+
+const ERC20ABI = [
+    "function decimals() external view returns (uint8)"
+];
 
 const getCurrentSushiswapLiquidity = async(pairAddress, tokenAddress) => {
     let tokenLiquidity = "";
@@ -9,14 +13,20 @@ const getCurrentSushiswapLiquidity = async(pairAddress, tokenAddress) => {
         jsonABI.result,
         provider
     );
+    const ERC20 = new ethers.Contract(
+        tokenAddress,
+        ERC20ABI,
+        provider
+    )
     try{
+        const decimals = await ERC20.decimals();
         const token0 = await contract.token0();
         const token1 = await contract.token1();
         const result = await contract.getReserves();
         if(tokenAddress === token0){
-            tokenLiquidity = ethers.utils.formatUnits(result._reserve0, 18);
+            tokenLiquidity = ethers.utils.formatUnits(result._reserve0, decimals);
         } else if(tokenAddress === token1){
-            tokenLiquidity = ethers.utils.formatUnits(result._reserve1, 18);
+            tokenLiquidity = ethers.utils.formatUnits(result._reserve1, decimals);
         } else {
             throw "Token address does not match with any token in this pool!";
         }
