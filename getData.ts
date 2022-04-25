86329397d2121041dfa3ff582633f04b4805194d
Adam Akbar <adam.akbar@ruangguru.com>
2022-04-25 16:51:51 +0700

    get voter data add before date

--- getData.ts
+++ getData.ts
@@ -1,7 +1,7 @@
 "use strict";
 const axios = require('axios');
 const ObjectsToCsv = require('objects-to-csv');
-
+const dayjs = require('dayjs');
 
 const getData = new Promise(async function (resolve, reject) {
   const endpoint = "https://hub.snapshot.org/graphql";
@@ -15,10 +15,12 @@ const getData = new Promise(async function (resolve, reject) {
     try {
       const graphqlQuery = {
         "operationName": "Votes",
-        "query": `query Votes($id: String!, $first: Int, $skip: Int, $orderBy: String, $orderDirection: OrderDirection, $voter: String) {\n  votes(\n    first: $first\n    skip: $skip\n    where: {proposal: $id, vp_gt: 0, voter: $voter}\n    orderBy: $orderBy\n    orderDirection: $orderDirection\n  ) {\n        voter\n    choice\n      }\n}`,
+        "query": `query Votes($id: String!, $first: Int, $skip: Int, $orderBy: String, $orderDirection: OrderDirection, $voter: String) {\n  votes(\n    first: $first\n    skip: $skip\n    where: {proposal: $id, vp_gt: 0, voter: $voter}\n    orderBy: $orderBy\n    orderDirection: $orderDirection\n  ) {\n        voter\n    choice\n  created    }\n}`,
         "variables": {
           "id": "0xdd1c21a41e63df9f23d8ad99c13af492440cc58047f6117dd5021ccec97018bf",
           "first": 10,
+          "orderBy": "created",
+          "orderDirection": "asc",
           "skip": i // get data from skip to skip + 10
         }
       };
@@ -28,7 +30,7 @@ const getData = new Promise(async function (resolve, reject) {
         headers: headers,
         data: graphqlQuery
       });
-      if (response.data.data.votes) { // check if there is data
+      if (response.data.data.votes && i <= 20) { // check if there is data
         data.push(response.data.data.votes);
         i += 10;
       }
@@ -49,10 +51,20 @@ const getData = new Promise(async function (resolve, reject) {
 type Vote = {
   voter: string;
   choice: number;
-
+  created: number;
+  isBeforeDate: number;
 }
 const resolveData = async () => {
   const data = await getData as Array<Vote>;
+  data.map(vote => {
+    if (dayjs.unix(vote.created).isBefore(dayjs.unix(1650009900))) {
+      vote.isBeforeDate = 1;
+
+    }
+    else {
+      vote.isBeforeDate = 0;
+    }
+  })
   const csv = new ObjectsToCsv(data);
   await csv.toDisk('./voteResult.csv');
 
